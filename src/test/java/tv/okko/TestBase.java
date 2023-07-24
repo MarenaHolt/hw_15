8508d358b0a942d890bfadd6f9c77a21ab2380da
Marena <fabriana.sands@gmail.com>
2023-07-24 20:20:48 +0500

    add tests

---
+++ src/test/java/tv/okko/TestBase.java
@@ -0,0 +1,56 @@
+package tv.okko;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import tv.okko.helpers.Attach;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+        @BeforeAll
+        static void beforeAll() {
+            //  Configuration.baseUrl = "https://demoqa.com";
+            //  Configuration.browserSize = "1920x1080";
+            //Configuration.browser = "chrome";
+            // Configuration.holdBrowserOpen = true;
+            Configuration.pageLoadStrategy = "eager";
+            //  Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+            Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+            Configuration.baseUrl = System.getProperty("baseUrl","https://okko.tv/");
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+            Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            Configuration.browserCapabilities = capabilities;
+        }
+
+        @BeforeEach
+        void addListener() {
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        }
+
+        @AfterEach
+        void addAttachments() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+
+            closeWebDriver();
+        }
+}
---
+++ src/test/java/tv/okko/pages/MainPage.java
@@ -0,0 +1,74 @@
+package tv.okko.pages;
+
+import com.codeborne.selenide.*;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainPage {
+    private SelenideElement
+            searchBar = $("[test-id='nav_search']"),
+            inputTypeSearch = $("[type='search']"),
+            resultSearchHeader = $(".uyjr3HkY"),
+            promoCode = $("[test-id='nav_promo']"),
+            promoInput = $("[test-id='promo_input']"),
+            promoActivate = $("[test-id='promo_activate']"),
+            imageLogo = $(".Vjha6F_C");
+
+    private static ElementsCollection resultElementsHeaders = $$(By.className("ewmlf9Mj"));
+
+    @Step("Открываем главную страницу")
+    public MainPage openMainPage() {
+        open("https://okko.tv/");
+        return this;
+    }
+
+    @Step("Получаем и проверяем элементы в хедере")
+    public MainPage checkHeaderMainPage() {
+        resultElementsHeaders.shouldHave(
+                CollectionCondition.texts("Главная", "Каталог",
+                        "Магазин", "Спорт", "ТВ каналы", "", "Месяц за 1 ₽", "Ввести промокод",
+                        "Войти"));
+        return this;
+    }
+
+    @Step
+    public MainPage checkButtonPromoCode() {
+        promoCode.shouldHave(Condition.visible).click();
+        return this;
+    }
+
+    public MainPage checkPromoInputEnabled(String promoCode) {
+        promoInput.shouldHave(Condition.visible).setValue(promoCode);
+        promoActivate.shouldHave(Condition.enabled);
+        return this;
+
+    }
+
+    public MainPage checkPromoInputDisabled() {
+        assertThat(promoInput.shouldHave(Condition.visible).getAttribute("placeholder")).isEqualTo("Промокод");
+        promoActivate.shouldHave(Condition.disabled);
+        return this;
+    }
+
+    public MainPage checkSearchButton() {
+        searchBar.click();
+        return this;
+    }
+
+    public MainPage checkInputTypeSearchPlaceholder() {
+        assertThat(inputTypeSearch.shouldHave(Condition.visible).getAttribute("placeholder")).isEqualTo("Название фильма, сериала или имя актёра, режиссёра");
+        return this;
+    }
+
+    public MainPage checkImgLogo() {
+        assertTrue(imageLogo.isImage());
+        assertThat(imageLogo.getAttribute("src")).isEqualTo("https://static.okko.tv/notifications/tv/1682682482088/1682682502609_512x204.png");
+        return this;
+    }
+
+}
